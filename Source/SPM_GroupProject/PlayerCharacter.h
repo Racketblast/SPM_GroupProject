236fab71295b6106fbf4b1d5d84465aae97ff200
haraldholmqvist <104203829+haraldholmqvist@users.noreply.github.com>
2025-04-14 09:51:18 +0200

    First Commit Test
    added player character class, and custom game mode asset

---
+++ Source/SPM_GroupProject/PlayerCharacter.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class SPM_GROUPPROJECT_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	//UPROPERTY(EditAnywhere)
+	//USceneComponent* Hand;
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(EditAnywhere)
+	UCameraComponent* PlayerCamera;
+	void MoveForward(float InputVector);
+	void MoveRight(float InputVector);
+	void Yaw(float InputVector);
+	void Pitch(float InputVector);
+	void Shoot();
+	
+
+	
+	//UPROPERTY(EditAnywhere)
+	//TSubclassOf<class AProjectile> ProjectileActor;
+};
