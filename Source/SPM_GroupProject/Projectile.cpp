7ebab920843581e865e3f71119ebd9b9272502db
Gabelito <104159249+Gabelito@users.noreply.github.com>
2025-04-15 10:00:09 +0200

    merge1

---
+++ Source/SPM_GroupProject/Projectile.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "projectile.h"
+
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// Sets default values
+AProjectile::AProjectile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>("ProjectileMesh");
+	RootComponent = ProjectileMesh;
+	ProjectileComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComponent"));
+	ProjectileComponent-> InitialSpeed = 900.f; 
+}
+
+// Called when the game starts or when spawned
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	SetLifeSpan(TCount);
+}
+
+// Called every frame
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (TCount <= 0 )
+	{
+		Destroy();
+	}
+
+}
+
