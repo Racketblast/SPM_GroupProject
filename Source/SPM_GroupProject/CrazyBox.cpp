dbabcd06c74dbf6c1439a8cf9c455515cd846201
Racketblast <144691527+Racketblast@users.noreply.github.com>
2025-05-11 02:23:11 +0200

    Moved classes and "crazy?"

---
+++ Source/SPM_GroupProject/CrazyBox.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CrazyBox.h"
+
+#include "PlayerGameInstance.h"
+
+// Sets default values
+ACrazyBox::ACrazyBox()
+{
+	CubeMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("CubeMesh");
+}
+
+void ACrazyBox::Use_Implementation(APlayerCharacter* Player)
+{
+	if (UPlayerGameInstance* GI = Cast<UPlayerGameInstance>(GetGameInstance()))
+	{
+		if (bCrazy)
+		{
+			GI->NextDialogueRowName = "";
+			bCrazy = false;
+		}
+		else
+		{
+			GI->StartDialogueRowName = "Crazy?";
+			GI->StartDialogue();
+			bCrazy = true;
+		}
+	}
+}
+
---
+++ Source/SPM_GroupProject/CrazyBox.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerUseInterface.h"
+#include "GameFramework/Actor.h"
+#include "CrazyBox.generated.h"
+
+UCLASS()
+class SPM_GROUPPROJECT_API ACrazyBox : public AActor, public IPlayerUseInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACrazyBox();
+	virtual void Use_Implementation(APlayerCharacter* Player) override;
+
+protected:
+	UPROPERTY(EditAnywhere)
+	class UStaticMeshComponent* CubeMeshComponent;
+
+	bool bCrazy = false;
+};
