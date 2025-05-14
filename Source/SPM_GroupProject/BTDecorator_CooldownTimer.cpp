ba9532d3627e3211e9e6aefca7a5ccd86090baad
Whammar04 <whammar04@gmail.com>
2025-05-14 15:47:23 +0200

    Fixade mer med flygande fienden

---
+++ Source/SPM_GroupProject/BTDecorator_CooldownTimer.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTDecorator_CooldownTimer.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTDecorator_CooldownTimer::UBTDecorator_CooldownTimer()
+{
+	NodeName = "Cooldown Timer";
+}
+
+bool UBTDecorator_CooldownTimer::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	if (auto* Blackboard = OwnerComp.GetBlackboardComponent())
+	{
+		float LastTime = Blackboard->GetValueAsFloat(TimeKey);
+		float CurrentTime = OwnerComp.GetWorld()->GetTimeSeconds();
+		return (CurrentTime - LastTime) >= Cooldown;
+	}
+	return false;
+}
